e383bf617e7e1fd78f8fe2a22f4e659dd2b8d812
German <gescandar@fi.uba.ar>
2023-05-23 19:32:02 -0300

    restaurar MatchState original

--- common_src/matchstate.h
+++ common_src/matchstate.h
@@ -1,27 +1,18 @@
-#ifndef _MATCHSTATE_H
-#define _MATCHSTATE_H
-
+#ifndef MATCHSTATE_H
+#define MATCHSTATE_H
 #include <string>
-#include <vector>
-
-enum direction {
-    LEFT,
-    RIGHT
-};
 
 struct MatchState {
-    uint8_t direction;
-    bool is_shooting;
-    uint8_t rounds;
-    uint16_t hit_points;
-    std::vector<int> position;
+    std::string command;
+    int match_code;
+    bool succeeded;
+    std::string message;
 
+    MatchState() = default;
     MatchState(
-    uint8_t direction,
-    bool is_shooting,
-    uint8_t rounds,
-    uint16_t hit_points,
-    int x, int y) : 
-        direction(direction), is_shooting(is_shooting), rounds(rounds), hit_points(hit_points), position{x,y} {}
+    const std::string &cmd, 
+    int code, bool success, 
+    const std::string &msg = "") : 
+        command(cmd), match_code(code), succeeded(success), message(msg) {}
 };
 #endif
