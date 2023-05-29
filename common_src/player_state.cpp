33792a83e32f30942e797db4548345f2df9c5a97
German <gescandar@fi.uba.ar>
2023-05-28 23:42:35 -0300

    crear struct PlayerStateReference, que representa los datos del jugador que se devolveran al cliente

--- common_src/player_state.cpp
+++ common_src/player_state.cpp
@@ -1,12 +1,30 @@
 #include "player_state.h"
+#include "./client/client_protocol.h"
 #include <arpa/inet.h>
 
-PlayerState::PlayerState(int8_t max_x, int8_t max_y) : direction(2, 0), position(2, 0) {
+PlayerState::PlayerState(const std::string &name, int8_t max_x, int8_t max_y) : 
+name(name), direction(2, 0), position(2, 0) {
     this->rounds = GUN_MAGAZINE_SIZE;
     this->state = IDLE;
     this->max_x = max_x;
     this->max_y = max_y;
     this->hit_points = STARTING_HIT_POINTS;
+}
+
+std::string PlayerState::get_name() {
+    return name;
+}
+
+PlayerStateReference PlayerState::make_ref() {
+    PlayerStateReference ref;
+    ref.hit_points = hit_points;
+    ref.name = name;
+    ref.rounds = rounds;
+    ref.state = state;
+    ref.x = position[0];
+    ref.y = position[1];
+    ref.direction = direction[0] == 1 ? RIGHT : LEFT;
+    return ref;
 }
 
 void PlayerState::move() {
@@ -69,14 +87,14 @@ void PlayerState::shoot(int flag) {
     }
 }
 
-void PlayerState::next_state(const ProtocolRequest &req) {
-    if (req.move == MOVE_DOWN) {
+void PlayerState::next_state(int cmd) {
+    if (cmd == MOVE_DOWN) {
         direction[1] = 1;
-    } else if (req.move == MOVE_UP) {
+    } else if (cmd == MOVE_UP) {
         direction[1] = -1;
-    } else if (req.move == MOVE_RIGHT) {
+    } else if (cmd == MOVE_RIGHT) {
         direction[0] = 1;
-    } else if (req.move == MOVE_LEFT) {
+    } else if (cmd == MOVE_LEFT) {
         direction[0] = -1;
     }
     this->move();
