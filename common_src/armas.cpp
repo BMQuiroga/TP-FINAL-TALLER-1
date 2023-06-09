9dc1f2ec0878e3e3f0a4a5d3535e391a3124f8c1
BMQuiroga <bquiroga@fi.uba.ar>
2023-06-09 17:17:49 -0300

    me habia faltado agregar archivos de armas

---
+++ common_src/armas.cpp
@@ -0,0 +1,61 @@
+#include "armas.h"
+#include "player_state.h"
+
+bool Arma::try_reload() {
+    std::cout << "try reload" << std::endl;
+    std::cout << "dr: " << std::to_string(this->delay_recarga)<< std::endl;
+    std::cout << "dd: " << std::to_string(this->delay_disparo)<< std::endl;
+    if (delay == 0 && balas != cargador) {
+        std::cout << "reload" << std::endl;
+        balas = cargador;
+        delay = delay_recarga;
+        return true;
+    }
+    return false;
+}
+
+bool Arma::try_shoot() {
+    std::cout << "try shoot" << std::endl;
+    if (delay == 0 && balas > 0) {
+        std::cout << "shoot" << std::endl;
+        balas--;
+        delay = delay_disparo;
+        this->create_bullet();
+        return true;
+    }
+    return false;
+}
+
+
+uint8_t Arma::get_rounds() {
+    return this->balas;
+}
+
+void Arma::advance_time() {
+    std::cout << "delay era:" << std::to_string(delay) << std::endl;
+    if (this->delay > 0) {
+        this->delay--;
+        std::cout << "nuevo delay:" << std::to_string(delay) << std::endl;
+    }
+        
+}
+
+void Arma1::create_bullet() {
+    //uint16_t x = this->owner->position.at(0);
+    //uint16_t y = this->owner->position.at(1);
+    //uint8_t direction = this->owner->facing_direction;
+
+}
+
+Arma::Arma(uint8_t c, uint8_t dr, uint8_t dd, PlayerState * owner) {
+    this->cargador = c;
+    this->balas = c;
+    this->delay_recarga = dr;
+    this->delay_disparo = dd;
+    this->delay = 0;
+    this->owner = owner;
+}
+
+Arma1::Arma1(PlayerState * owner) : Arma(10,20,2,owner) {}
+
+
---
+++ common_src/armas.h
@@ -0,0 +1,34 @@
+#ifndef __ARMA_GENERICA_H__
+#define __ARMA_GENERICA_H__
+
+#include <iostream>
+#include "player_state.h"
+
+class PlayerState;
+
+class Arma {
+    protected:
+    //public:
+    uint8_t cargador;//CONSTANTE, BALAS QUE TIENE 1 CARGADOR
+    uint8_t balas;
+    uint8_t delay_recarga;//CONSTANTE, TIEMPO QUE TARDA EN RECARGAR
+    uint8_t delay_disparo;//CONSTANTE, TIEMPO ENTRE DISPAROS
+    uint8_t delay;
+    PlayerState * owner;
+
+    public:
+    Arma(uint8_t c, uint8_t dr, uint8_t dd, PlayerState * owner);
+    void advance_time();
+    bool try_shoot();
+    bool try_reload();
+    uint8_t get_rounds();
+    virtual void create_bullet() = 0;
+};
+
+class Arma1 : public Arma {
+    public:
+    Arma1(PlayerState * owner);
+    void create_bullet() override;
+};
+
+#endif
