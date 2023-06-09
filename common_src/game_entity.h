19f764fa0532d4e7b842a883b774c9d9823448cd
German <gescandar@fi.uba.ar>
2023-06-09 11:55:05 -0300

    agregar clase GameEntity, clase base para que hereden las clases del jugador y los zombies

---
+++ common_src/game_entity.h
@@ -0,0 +1,51 @@
+#ifndef _GAME_ENTITY_H
+#define _GAME_ENTITY_H
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+
+// 0 idle, 1 attack, 2 dead, 3 grenade, 4 hurt 5 recharge, 6 shot, 7 walk, 
+// 8 fall, 9 run, 10 protect, 11 run+atack, 12 bite, 13 scream, 14 eating
+enum entity_state { 
+  IDLE, 
+  ATTACKING, 
+  DEAD, 
+  THROWING_GRENADE, 
+  HURT, 
+  RELOADING, 
+  SHOT, 
+  MOVING, 
+  FALLING, 
+  RUNNING, 
+  BLOCKING, 
+  ATTACKING_AND_MOVING, 
+  BITING, 
+  SCREAMING, 
+  EATING 
+  };
+enum entity_direction { LEFT, RIGHT };
+
+class GameEntity {
+  protected:
+    static int next_id; // Static member variable to track the next ID
+    int id; // Instance-specific ID
+    std::string name;
+    uint16_t x;
+    uint16_t y;
+    std::vector<int8_t> direction;
+    entity_direction facing_direction;
+    uint16_t speed;
+    int8_t state;
+    int16_t max_x, max_y;
+    void move();
+    virtual void attack() = 0;
+
+  public:
+    GameEntity(const std::string &name, int16_t max_x, int16_t max_y);
+    ~GameEntity();
+    std::string get_name();
+};
+
+#endif
--- common_src/player_state.cpp
+++ common_src/player_state.cpp
@@ -2,8 +2,12 @@
 #include "./client/client_protocol.h"
 #include <arpa/inet.h>
 
-PlayerState::PlayerState(const std::string &name, int16_t max_x, int16_t max_y) : 
-name(name), direction(2, 0), position(2, 0), facing_direction(RIGHT), speed(5) {
+PlayerState::PlayerState(
+    const std::string &name, 
+    int16_t max_x, 
+    int16_t max_y
+) : 
+GameEntity(name, max_x, max_y) {
     this->id = 1;
     this->rounds = GUN_MAGAZINE_SIZE;
     this->state = IDLE;
@@ -23,36 +27,25 @@ PlayerStateReference PlayerState::make_ref() {
     ref.name = name;
     ref.rounds = rounds;
     ref.state = state;
-    ref.x = position[0];
-    ref.y = position[1];
+    ref.x = x;
+    ref.y = y;
     ref.direction = facing_direction;
     return ref;
 }
 
+void PlayerState::attack() {}
+
 void PlayerState::move() {
-    std::vector<uint16_t> prev_position(this->position);
-    int16_t new_x = (int16_t)this->position[0] + (speed * this->direction[0]);
-    if (new_x < 0) {
-        new_x = 0;
-    } else if (new_x >= this->max_x) {
-        new_x = this->max_x - 1;
-    }
-    int16_t new_y = (int16_t)this->position[1] + (speed * this->direction[1]);
-    if (new_y < 0) {
-        new_y = 0;
-    } else if (new_y >= this->max_y) {
-        new_y = this->max_y - 1;
-    }
-    this->position[0] = new_x;
-    this->position[1] = new_y;
+    std::vector<uint16_t> prev_position({x, y});
+    GameEntity::move();
 
     bool moved = false;
     if (this->direction[0] != 0) {
-        if (prev_position[0] != this->position[0]) {
+        if (prev_position[0] != x) {
             moved = true;
         }
     } else if (this->direction[1] != 0) {
-        if (prev_position[1] != this->position[1]) {
+        if (prev_position[1] != y) {
             moved = true;
         }
     }
@@ -72,24 +65,24 @@ void PlayerState::move() {
     }
 }
 
-void PlayerState::shoot(int flag) {
-    if (flag && this->rounds > 0) {
-        this->rounds--;
-        if (this->state == MOVING) {
-            this->state = ATTACKING_AND_MOVING;
-        } else if (this->state == IDLE) {
-            this->state = ATTACKING;
-        }
-    } else {
-        if (this->state == ATTACKING) {
-            this->state = IDLE;
-        } else if (this->state == ATTACKING_AND_MOVING) {
-            this->state = MOVING;
-        }
-    }
-}
+// void PlayerState::shoot(int flag) {
+//     if (flag && this->rounds > 0) {
+//         this->rounds--;
+//         if (this->state == MOVING) {
+//             this->state = ATTACKING_AND_MOVING;
+//         } else if (this->state == IDLE) {
+//             this->state = ATTACKING;
+//         }
+//     } else {
+//         if (this->state == ATTACKING) {
+//             this->state = IDLE;
+//         } else if (this->state == ATTACKING_AND_MOVING) {
+//             this->state = MOVING;
+//         }
+//     }
+// }
 
-void PlayerState::next_state(int cmd) {
+void PlayerState::next_state(uint8_t cmd) {
     if (cmd == MOVE_DOWN) {
         direction[1] = 1;
     } else if (cmd == MOVE_UP) {
@@ -110,46 +103,6 @@ void PlayerState::next_state(int cmd) {
         direction[0] = 0;
     }
     this->move();
-    // switch (req->cmd) {
-    //     case NOP:
-    //         if (this->state == ATTACKING) {
-    //             this->shoot(1);
-    //         } else if (this->state == MOVING) {
-    //             this->move();
-    //         } else if (this->state == ATTACKING_AND_MOVING) {
-    //             this->move();
-    //             this->shoot(1);
-    //         }
-    //         break;
-
-    //     case SHOOT:
-    //         this->shoot(req->args[0]);
-    //         if (this->state == MOVING || this->state == ATTACKING_AND_MOVING) {
-    //             this->move();
-    //         }
-    //         break;
-
-    //     case MOVE:
-    //         this->direction[0] = req->args[0];
-    //         this->direction[1] = req->args[1];
-    //         this->move();
-    //         if (this->state == ATTACKING || this->state == ATTACKING_AND_MOVING) {
-    //             this->shoot(1);
-    //         }
-    //         break;
-
-    //     case RELOAD:
-    //         this->state = IDLE;
-    //         this->rounds = GUN_MAGAZINE_SIZE;
-    //         break;
-    //     }
 }
-
-// void PlayerState::as_response(protocol_response_t *response) {
-//     response->state = this->state;
-//     response->position[0] = htons(this->position[0]);
-//     response->position[1] = htons(this->position[1]);
-//     response->rounds = htons(this->rounds);
-// }
 
 PlayerState::~PlayerState() {}
