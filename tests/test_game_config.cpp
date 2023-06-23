9bea1f2837885f11c07dedb220b22e240fa3b6c3
German <gescandar@fi.uba.ar>
2023-06-22 23:23:54 -0300

    agregar prueba basica de GameConfig

---
+++ tests/test_game_config.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include "../common_src/game_config.h"
+
+TEST(GameConfigTest, BasicAssertions) {
+    GameConfig *instance = GameConfig::get_instance();
+    // Expect equality.
+    EXPECT_EQ(instance->get_value<int>("PLAYER_HP"), 100);
+}
